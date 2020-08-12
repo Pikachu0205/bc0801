67dc95ec2bced80cf8485f130ab28beb0f63eaaa
Pikachu0205 <12353eddie@gmail.com>
2020-08-12 17:39:01 +0800

    100transaction取前5個

--- S8-5.js
+++ S8-5.js
@@ -14,7 +14,7 @@ myDeliver = require("./myDeliver.js");
 
 app.post('/Height',upload.array(), function(req, res) {
 	
-	transaction123 = req.body.transaction.slice(0, 91);
+	transaction123 = req.body.transaction.slice(0, 5);
 	//if(req.body.height == height + 1)
 		myMain.newHeight(0);
 	

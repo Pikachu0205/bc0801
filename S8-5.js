3d64a08840475e1b30b512015e21186e470ceb0c
Pikachu0205 <12353eddie@gmail.com>
2020-08-19 13:19:13 +0800

    90

--- S8-5.js
+++ S8-5.js
@@ -14,10 +14,11 @@ myDeliver = require("./myDeliver.js");
 
 app.post('/Height',upload.array(), function(req, res) {
 	
-	//transaction123 = req.body.transaction.slice(0, 90);
-	transaction123 = req.body.transaction;
+	transaction123 = req.body.transaction.slice(0, 90);
+	//transaction123 = req.body.transaction;
 	//if(req.body.height == height + 1)
 		myMain.newHeight(0);
+	//height = req.body.blockHeight;
 	
 	console.log(transaction123);
 	

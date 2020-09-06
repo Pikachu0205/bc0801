938479e02dd8c9ceb3f8bbab3472ae59c4c4189c
Pikachu0205 <12353eddie@gmail.com>
2020-09-06 16:13:01 +0800

    紀錄書羅噗

--- S8-5.js
+++ S8-5.js
@@ -29,6 +29,7 @@ app.post('/Height',upload.array(), function(req, res) {
 	
 	console.log("req.body.height : " + req.body.height);
 	console.log("height : " + height);
+	console.log("transactionReceiver" + transactionReceiver);
 	if(transactionReceiver == 0 && height < req.body.height){
 	//if( !heighted.includes(req.body.height) ){
 		console.log("\n" + heighted);
